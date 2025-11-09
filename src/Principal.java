51763d0cd611484f6cb6ea518ee05bf52c0e9abc
Ander <Andercabreram@gmail.com>
2025-11-09 12:13:57 -0500

    Inicializando mi repositorio

---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Bus> buses = new ArrayList<>();
+        ArrayList<Pago> pagos = new ArrayList<>();
+        ArrayList<Ruta> rutas = new ArrayList<>();
+        ArrayList<Inspector> inspectores = new ArrayList<>();
+        ArrayList<ActaFiscalizacion> actas = new ArrayList<>();
+    
+    }
+}
