2b852cfd1146a8ddc87ebb3b381ada4d93b9af31
Ander <Andercabreram@gmail.com>
2025-11-09 12:35:20 -0500

    Creando clase Ruta

---
+++ src/Ruta.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Ruta {
+    private int idRuta;
+    private String nombreRuta;
+    private String frecuencia;
+    private ArrayList<Paradero> paraderos;
+}
