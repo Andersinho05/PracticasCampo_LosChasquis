0fc515623f0fe4e3094171f13e7cf62844892bb3
Rodrigo Carrillo <n00408695@upn.pe>
2025-11-09 23:18:06 -0500

    mi aporte en acta fiscalizacion

--- src/ActaFiscalizacion.java
+++ src/ActaFiscalizacion.java
@@ -5,10 +5,44 @@ public class ActaFiscalizacion {
     String evidencia;
     public ActaFiscalizacion() {
     }
+
     public ActaFiscalizacion(int idActa, String fecha, String tipoInfraccion, String evidencia) {
         this.idActa = idActa;
         this.fecha = fecha;
         this.tipoInfraccion = tipoInfraccion;
         this.evidencia = evidencia;
-    }
+    }
+
+    public int getIdActa() {
+        return idActa;
+    }
+
+    public void setIdActa(int idActa) {
+        this.idActa = idActa;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipoInfraccion() {
+        return tipoInfraccion;
+    }
+
+    public void setTipoInfraccion(String tipoInfraccion) {
+        this.tipoInfraccion = tipoInfraccion;
+    }
+
+    public String getEvidencia() {
+        return evidencia;
+    }
+
+    public void setEvidencia(String evidencia) {
+        this.evidencia = evidencia;
+    }
+    
 }
